500a8798941c3ce865bf8964b54494c37adeae34
SamuelLorrain <samuel.lorrain@gmail.com>
2022-04-10 15:43:25 +0200

    add basic authentication

--- backend/Controllers/HomeController.cs
+++ backend/Controllers/HomeController.cs
@@ -1,7 +1,10 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication;
 using backend.Models;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication.Cookies;
 
 namespace backend.Controllers;
 
@@ -14,9 +17,32 @@ public class HomeController : Controller
         _logger = logger;
     }
 
-    public IActionResult Login()
+    public IActionResult Index()
     {
         return View();
+    }
+
+    [HttpGet("login")]
+    public IActionResult Login(string returnUrl)
+    {
+        ViewData["ReturnUrl"] = System.Web.HttpUtility.UrlEncode(returnUrl);
+        return View();
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> ValidateAsync(string username, string password, string returnUrl)
+    {
+        if (username == "foo" && password == "bar") {
+            List<Claim> claims = new List<Claim>();
+            claims.Add(new Claim("username", username));
+            claims.Add(new Claim(ClaimTypes.NameIdentifier, username));
+            var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+            var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+            await HttpContext.SignInAsync(claimsPrincipal);
+
+            return Redirect(returnUrl);
+        }
+        return BadRequest();
     }
 
     public IActionResult Register()
--- backend/Program.cs
+++ backend/Program.cs
@@ -1,7 +1,11 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options => options.LoginPath = "/login");
 
 var app = builder.Build();
 
@@ -13,15 +17,15 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
+/* app.UseHttpsRedirection(); */
 app.UseStaticFiles();
-
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Login}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}"
+);
 
 app.Run();
